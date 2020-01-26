188058faf9c9447397e108504a35a90f17b052d7
Sean Hellum <seanhellum45@gmail.com>
2020-01-26 04:40:49 +0000

    fix :hammer:

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <string>
 #include <bits/stdc++.h> 
+using namespace std;
 
 int main() {
     while(1) {
-        char* line = readline(">>> ");
-        add_history(line);
-        system(line);
+        string line = readline(">>> ");
+        if (line == "clearhist") {
+            clear_history();
+            continue;
+        }
+        if(!(line.length() == 0)) {
+            add_history(line.c_str());
+        }
+        system(line.c_str());
     }
 }
