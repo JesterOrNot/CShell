b0bd0bec3c445fdc78899dfdb03d107f6ead7ee7
Sean Hellum <seanhellum45@gmail.com>
2020-01-26 04:46:37 +0000

    add logging :hammer:

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string>
@@ -8,6 +9,7 @@ int main() {
     while(1) {
         string line = readline(">>> ");
         if (line == "clearhist") {
+            cout << "\033[0;32mHistory has been cleared ✔\033[0m\n";
             clear_history();
             continue;
         }
